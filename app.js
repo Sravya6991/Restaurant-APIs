f8b57db2b718474afc58760ce186f29a21a21ee9
Sravya <sravya.veeravalli123@gmail.com>
2023-10-17 02:42:36 +0530

    updated zomato

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const mongoURL = "mongodb://127.0.0.1:27017";
 
+require("dotenv").config();
+const stripe = require('stripe')("sk_test_51Nx2IESFreFK53KxHY5CGcVnqmtjYiyE5epcTYBEZy78ZBY4swXBtZsvh2l5HwJjX8ep9YRm9BppAHTV9znGNpYa00Ql9WSln6");
+
 // const data = require("./Data");
 
 let db
@@ -31,6 +34,16 @@ app.get("/quicksearch", (req, res) => {
         if(err) throw err;
         res.send(result)
     })
+});
+
+// get mquicksearch based on mealtype
+app.post("/quicksearch", (req, res)=> {
+    if(Array.isArray(req.body)){
+        db.collection("mealtypes").find({mealtype_id: {$in: req.body}}).toArray((err, result) => {
+            if(err) throw err;
+            res.send(result);
+        })
+    }
 })
 
 // get list of restaurants
@@ -41,16 +54,20 @@ app.get("/restaurantsList", (req, res) => {
     })
 })
 
+
 // get restaurants wrt city
 app.get("/restaurants", (req, res) => {
     let query = {}
     let stateId = Number(req.query.state_id)
     let mealId = Number(req.query.meal_id)
+    let restId = Number(req.query.restId)
 
     if(stateId) {
         query = {state_id: stateId}
     } else if(mealId) {
         query = {"mealTypes.mealtype_id": mealId}
+    } else if(restId) {
+        query = {restaurant_id: restId}
     }
 
     db.collection("restaurants").find(query).toArray((err,result) => {
@@ -64,24 +81,22 @@ app.get("/restaurants", (req, res) => {
 app.get("/restaurants/:mealId", (req, res) => {
     const mealId = Number(req.params.mealId)
     const cuisineId = Number(req.query.cuisineId)
-
     const lcost = Number(req.query.lcost)
     const hcost = Number(req.query.hcost)
+    const location = req.query.location
     let sort = 1
-    let page = Number(req.query.page)
+    let pageNo = Number(req.query.page)
 
     if(Number(req.query.sort)) {
         sort = Number(req.query.sort)
     }
 
-    page = page ? page : 1;
-    let itemsPerPage = 2;
-    let startIndex = itemsPerPage*page - itemsPerPage;
-    let lastIndex = itemsPerPage*page;
-    
-    // if(req.query.sort) {
-    //     sort = {cost: req.query.sort}
-    // }
+    pageNo = pageNo ? pageNo : 1;
+    // let itemsPerPage = 2;
+    // let startIndex = itemsPerPage*pageNo - itemsPerPage;
+    // let lastIndex = itemsPerPage*pageNo;
+
+//   const paginatedProducts = ""
 
     let query;
 
@@ -90,6 +105,13 @@ app.get("/restaurants/:mealId", (req, res) => {
             "mealTypes.mealtype_id": mealId
         }
     }
+    if(mealId && location) {
+        query = {
+            "mealTypes.mealtype_id": mealId,
+            "address": location
+        }
+    }
+
     if(mealId && sort<1) {
         query = {
             "mealTypes.mealtype_id": mealId
@@ -97,29 +119,26 @@ app.get("/restaurants/:mealId", (req, res) => {
         sort = sort
     }
 
-    if(mealId && page>1) {
-        query = {
-            "mealTypes.mealtype_id": mealId
-        }
-    }
-
     if(mealId && cuisineId) {
         query = {
             "mealTypes.mealtype_id": mealId,
             "cuisines.cuisine_id": cuisineId
         }
-    } else if(mealId && lcost && hcost) {
+    } 
+    if(mealId && lcost && hcost) {
         query = {
             "mealTypes.mealtype_id": mealId,
             $and: [{cost: {$gte: lcost, $lt: hcost} }]
         }
-    } else if(mealId && cuisineId && lcost && hcost) {
-        query = {
-            "cusines.cuisine_id": cuisineId,
-            "mealTypes.mealtype_id": mealId,
-            $and: [{cost: {$gt: lcost, $lt: hcost}}]
-        }
     }
+    if(mealId && cuisineId && lcost && hcost) {
+        query = {
+            "cuisines.cuisine_id": cuisineId,
+            "mealTypes.mealtype_id": mealId,
+            $and: [{cost: {$gte: lcost, $lt: hcost}}]
+        }
+    } 
+    
 
     // 1 => asc
     // -1 => desc
@@ -129,15 +148,11 @@ app.get("/restaurants/:mealId", (req, res) => {
       .sort({"cost": sort})
       .toArray((err,result) => {
         if(err) throw err
-        res.send(result)
-        // const filterResponse = result.slice(startIndex, lastIndex)
-        // res.send(filterResponse)
-        // if(page>1) {
-        //     const filterResponse = result.slice(startIndex, lastIndex)
-        //     res.send(filterResponse)
-        // } else {
-        //     res.send(result)
-        // }
+        if(pageNo) {
+            res.send(result)
+        } else {
+            res.send("Oopss, error while loading the page!!");
+        }
     })
 })
 
@@ -157,7 +172,6 @@ app.get("/details/:id", (req, res)=> {
 // menu of restaurants
 app.get("/menu/:restId", (req, res) => {
     const restId = Number(req.params.restId)
-
     db.collection("menu")
       .find({restaurant_id: restId})
       .toArray((err, result) => {
@@ -191,7 +205,7 @@ app.post("/placeorders", (req, res) => {
     })
 })
 
-// orders 
+// all orders 
 app.get("/orders", (req, res) => {
     db.collection("orders")
       .find()
@@ -238,10 +252,31 @@ app.put("/updateorders/:id", (req, res) => {
 // delete order
 app.delete("/deleteorders/:id", (req, res) => {
     const oid = Number(req.params.id)
-    db.collection("orders").deleteOne({orderId: oid}, (err,result) => {
+    db.collection("orders").deleteOne({order_id: oid}, (err,result) => {
         if(err) throw err
         res.send("Order deleted!!")
     }) 
+});
+
+//  stripe payments
+const calculateOrderAmount = (item) => {
+        return Number(item.cost)
+}
+
+app.post("/create-payment-intent", async(req, res) => {
+    const {product} = req.body;
+    console.log(product)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(product),
+        currency: "inr",
+        automatic_payment_methods: {
+            enabled: "true"
+        },
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
 })
 
 MongoClient.connect(mongoURL, (err, client) => {
